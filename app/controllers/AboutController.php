b61b880fc62677d62f424a9abb2438f77df3906e
Douglas Juliano Roldo <douglasroldo@gmail.com>
2019-04-26 19:51:19 -0300

    Primeira comitação e Configuração Inicial

---
+++ app/controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+class AboutController extends ControllerBase
+{
+    public function initialize()
+    {
+        $this->tag->setTitle('Sobre');
+        parent::initialize();
+    }
+
+    public function indexAction()
+    {
+    }
+}
